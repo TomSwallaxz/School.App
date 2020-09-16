11c43ce450742ba361bd84bf74189e32f58b5f7a
yosef tagawi <yosef.tagawi1@gmail.com>
2020-09-16 16:28:54 +0300

    yosef

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const users = [{
     id: 1,
     name: 'Itshak Maharat',
