cca0f618477eb7b36d1ca27d272afd07e2c2ed2e
yosef tagawi <yosef.tagawi1@gmail.com>
2020-09-16 16:08:15 +0300

    try change name again

--- routes/users.js
+++ routes/users.js
@@ -3,37 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-<<<<<<< HEAD
-router.get("/", function (req, res, next) {
-  const users = [
-    {
-      id: 1,
-      name: "John Doe",
-    },
-    {
-      id: 2,
-      name: "Tom Swalla",
-    },
-    
-    
-
-  ];
-=======
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   const users = [{
-    "id": 1,
-    "name": "Itshak Maharat "
+    id: 1,
+    name: 'Itshak Maharat',
   },
   {
-    "id": 2,
-    "name": "Tom Swalla"
+    id: 2,
+    name: 'Yosef Tagawi',
   },
-  {
-    "id": 3,
-    "name": "Yosef Tagawi"
-  }
-];
->>>>>>> f86bf8370aed9663f1b5822b96ec062049d2959a
+  ];
   res.send(users);
 });
 
