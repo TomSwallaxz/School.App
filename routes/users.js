9edb29f194aeed5816c9a67f1157db1413dab559
IsaacM <Itshak890@gmail.com>
2020-09-16 13:38:11 +0300

    itzhak commit

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 
 var express = require('express');
-=======
-var express = require("express");
->>>>>>> 18a042e31735746a35ad5a371cb5cfe021dccc19
 
 var router = express.Router();
 
 /* GET users listing. */
-<<<<<<< HEAD
 router.get('/', function(req, res, next) {
   const users = [{
     "id": 1,
@@ -16,22 +11,9 @@ router.get('/', function(req, res, next) {
   },
   {
     "id": 2,
-    "name": "Tom swallla"
+    "name": "Tom swalla"
   }
 ];
-=======
-router.get("/", function (req, res, next) {
-  const users = [
-    {
-      id: 1,
-      name: "John Doe",
-    },
-    {
-      id: 2,
-      name: "Tom Swalla",
-    },
-  ];
->>>>>>> 18a042e31735746a35ad5a371cb5cfe021dccc19
   res.send(users);
 });
 
