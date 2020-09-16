18a042e31735746a35ad5a371cb5cfe021dccc19
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-09-16 13:30:25 +0300

    my first commit

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const users = [{
-    "id": 1,
-    "name": "John Doe"
-  },
-  {
-    "id": 2,
-    "name": "Jane Doe"
-  }];
+router.get("/", function (req, res, next) {
+  const users = [
+    {
+      id: 1,
+      name: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Tom Swalla",
+    },
+  ];
   res.send(users);
 });
 
